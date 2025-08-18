72d502b5dcb642f00785831f80a706d5e3775926
krzysiekdz <krzysztof.dziezyk@gmail.com>
2025-08-18 15:08:49 +0200

    FEAT: my_flutter - main file

---
+++ lib/main2.dart
@@ -0,0 +1,26 @@
+// import 'package:flutter/widgets.dart';
+// import 'package:flutter_learn_2025/stoper/app.dart';
+
+// import 'package:flutter_learn_2025/my_flutter/widgets.dart';
+
+// zrobic wlasna warstwe widgetow - czyli korzystac z frameworka flutter, ale napisac wlasna warstwe widgetow
+// czyli wszedzie tam, gdzie framework odwoluje sie do widgetow - nadpisac to wlasnymi klasami i je wywolywac
+
+//root widget - tutaj tworzy sie hierarchia widgetow - powinny wiec dzialac elementy - monotwanie ich przez build ownera
+//w utworzonych plikach stopniowo dodawac implementacje (na razie to puste odwloania)
+
+void main() {
+  // runApp(Test());
+}
+
+// class Test extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     throw UnimplementedError();
+//   }
+
+//   @override
+//   Element createElement() {
+//     throw UnimplementedError();
+//   }
+// }
